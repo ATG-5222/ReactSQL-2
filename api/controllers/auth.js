6c2ff2144cbb0056e46789a7c87ece268a56f046
ATG-5222 <A01275222@tec.mx>
2023-02-26 20:30:09 -0600

    Creación de jwt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req,res) => {
     //CHECK EXISTING USER
@@ -30,9 +31,18 @@ export const login = (req,res) => {
         if(err) return res.json(err);
         if(data.lenght===0) return res.status(404).json("User not found");
         //CHECK PASSWORD
-        const isPasswordCorrect = bcrypt.compareSync(req.body.password,data[0]);
+        const isPasswordCorrect = bcrypt.compareSync(
+            req.body.password,
+            data[0].password
+        );
         if(!isPasswordCorrect) return res.status(404).json("Wrong username or password");
-    })
+        //Generate jsonwebtoken
+        const token = jwt.sign({id:data[0].id_user},"jwtkey");
+        const { password,...other } = data[0];
+        res.cookie("access_token",token,{
+            httpOnly:true
+        }).status(200).json(other);
+    });
 };
 
 export const logout = (req,res) => {
--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import { authRouter } from "./routes/auth.js";
 import { usersRouter } from "./routes/users.js";
 import { postRouter } from "./routes/posts.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRouter);
 app.use("/api/users",usersRouter);
 app.use("/api/posts",postRouter);
