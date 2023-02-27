2a455a5d6d68bfc2da039ca960dc820b98a9c0d4
ATG-5222 <A01275222@tec.mx>
2023-02-26 18:38:02 -0600

    Creación inicial de rutas

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import { authRouter } from "./routes/auth.js";
+import { usersRouter } from "./routes/users.js";
+import { postRouter } from "./routes/posts.js";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth",authRouter);
+app.use("/api/users",usersRouter);
+app.use("/api/posts",postRouter);
 
 app.listen(8800,()=>{
-    console.log("Coonected to the backend")
+    console.log("Connected to the backend")
 });
---
+++ api/routes/users.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+export const usersRouter = express.Router();
+
+//postRouter.get("/test",addPost);
